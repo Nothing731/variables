b8f23f6e70f7a691580e567ef2473cf0e56eaa74
Nothing731 <republicofgamers378@gmail.com>
2024-10-13 12:59:00 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var user = prompt('Enter you name...', `Bahodir`) // string
+var age  = +prompt('Enter your age...', 14) // number
+
+// alert('My name is ' + user);
+alert(`My name is ${user}, I'm ${age} years old.`)
+console.log(5 + 5);
